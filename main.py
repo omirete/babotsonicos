16dc25a43cbc84e6b02ba791478e8124a1986af1
Olga Mirete <olgamirete@gmail.com>
2023-04-18 20:07:52 +0200

    added argument parsing for using the script as a command line tool

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
+import argparse
+from argparse import Namespace
 from tweetipy import Tweetipy
 from json import load
 import random
-from os import getenv
 
-with open('phrases.json', 'r', encoding='utf-8') as f:
-    phrases: dict = load(f)
+def parseArgs() -> Namespace:
+    parser = argparse.ArgumentParser(
+        prog='Twitter bot: @babotsonicos',
+        description="Picks a random Babaosnicos' song and tweets a random phrase from it.",
+        epilog='For questions, reach out to the developer (GitHub user: /omirete).')
+
+    parser.add_argument('--api_key', required=True)
+    parser.add_argument('--api_secret', required=True)
+    parser.add_argument('--oauth_key', required=True)
+    parser.add_argument('--oauth_secret', required=True)
+    parser.add_argument('--text', required=False)
+
+    return parser.parse_args()
 
 def getRandomText() -> str:
-    songs = list(phrases.keys())
-    song = random.choice(songs)
-    phrase = random.choice(phrases[song])
-    return phrase
+    with open('phrases.json', 'r', encoding='utf-8') as f:
+        phrases: dict = load(f)
+        songs = list(phrases.keys())
+        song = random.choice(songs)
+        phrase = random.choice(phrases[song])
+        return phrase
 
+def postTweet(api_key: str, api_secret: str, oauth_key: str, oauth_secret: str, text: str = None):
 
-API = Tweetipy(
-    getenv("TWITTER_API_KEY"),
-    getenv("TWITTER_API_KEY_SECRET")
-)
+    API = Tweetipy(
+        oauth_consumer_key=api_key, # TWITTER_API_KEY
+        oauth_consumer_secret=api_secret, # TWITTER_API_KEY_SECRET
+        oauth_token=oauth_key,
+        oauth_token_secret=oauth_secret
+    )
+    
+    phrase = text if text != None else getRandomText()
+    API.tweets.write(phrase)
 
-phrase = getRandomText()
-API.tweets.write(phrase)
+def main():
+    args = parseArgs()
+    postTweet(**vars(args))
+
+main()
