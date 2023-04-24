8d9b9174c70a5c2cbb04c1f033aed11e131fa310
Olga Mirete <olgamirete@gmail.com>
2023-04-24 20:16:07 +0200

    write tweet first, then wait

--- reply_to_mentions.py
+++ reply_to_mentions.py
@@ -67,9 +67,9 @@ def main():
             # Let's try to be good web citizens and wait at least one minute
             # between each call to the API so we don't hit too hard on Twitter's
             # servers. This is also helpful if we want to avoid being banned. 😅
-            sleep(60)
             API.tweets.write(phrase, in_reply_to_tweet_id=t.id)
             id_of_last_tweet_replied_to = t.id
+            sleep(60)
         except Exception as e:
             # Log error
             log_error(str(e))
